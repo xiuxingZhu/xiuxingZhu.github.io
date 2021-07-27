92c876b3f707e85ac5c8d94f0a8308eda8dec888
xiuxingZhu <87921838+xiuxingZhu@users.noreply.github.com>
2021-07-27 14:28:18 +0800

    Update thumvnail.js

--- thumvnail.js
+++ thumvnail.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    let targetImage = document.querySelector("#smart-image");
+    targetImage.addEventListener("click", function() {
+    if (targetImage.classList.contains("small")) {
+    targetImage.classList.remove("small");
+} else {
+    targetImage.classList.add("small");
+}        
+});
+});
 
