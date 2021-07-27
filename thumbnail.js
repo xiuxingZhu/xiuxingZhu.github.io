2f22948b46a01efa4c5f93a11d1ed8e0dc4d7dcd
xiuxingZhu <87921838+xiuxingZhu@users.noreply.github.com>
2021-07-27 19:35:20 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,12 +1,12 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    let targetImage = document.querySelector("#smart-image");
-    targetImage.addEventListener("click", function() {
-    if (targetImage.classList.contains("small")) {
-    targetImage.classList.remove("small");
-} else {
-    targetImage.classList.add("small");
-}        
-});
+document.addEventListener("DOMContentLoaded",function(event){
+    let targetlmage=document.querySelector("#smart-image");
+    targetlmage.addEventListener("click",function(){
+        if(targetlmage.classList.contains("small");
+           targetlmage.classList.remove("small");
+    }else{
+                                 targetlmage.classList.add("small")
+}
+                          });
 });
 
 
