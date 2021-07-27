0cb87c9b31d7164ed9822e1b55bcb77c39aeb65b
xiuxingZhu <87921838+xiuxingZhu@users.noreply.github.com>
2021-07-27 18:42:48 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -2,8 +2,13 @@ document.addEventListener("DOMContentLoaded", function(event)
                           {
     
     let targetImage = document.querySelector("#smart-image");
+   
+  targetImage.addEventListener("click", function() {
     
-    targetImage.addEventListener("click", function() 
+    alert("点击了图片");
+
+  });
+
                                  {
     
         if (targetImage.classList.contains("small")) 
